cf217f55f0be77a84c4e18ccda43f94e938d0cbd
Gaurav prajapati <g.m.prajapati9827@gmail.com>
2024-09-03 02:02:23 +0530

    Project and framework setup for BargainHunterJS Test Automation Project

---
+++ test scripts/amazon.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+import { amazonIN } from '../utils/enviorment';
+
+test("amazon tests",async({page})=>{
+
+    await page.goto(amazonIN);
+});
---
+++ utils/enviorment.js
@@ -0,0 +1,2 @@
+export const amazonIN ="https://www.amazon.in";
+export const flipkartCOM ="https://www.flipkart.com";
