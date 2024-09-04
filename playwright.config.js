efeb875f5b23a6dd757f5a27f727aecc05413218
Gaurav prajapati <g.m.prajapati9827@gmail.com>
2024-09-05 02:11:12 +0530

    added the amazon product and search,addtocart tests

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below.*/
   use: {
     headless:false,
-    screenshot:'on',
+    // screenshot:'on',
     /* Collect trace when retrying the failed test.*/
     trace: 'retain-on-failure',
   },
